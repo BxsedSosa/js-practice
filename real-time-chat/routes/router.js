321bd5944d37e91e76469e052f29a505b3c50182
BxsedSosa <donovanxabad@gmail.com>
2025-02-10 09:55:29 -0800

    working on making the chat page

--- real-time-chat/routes/router.js
+++ real-time-chat/routes/router.js
@@ -1,9 +1,15 @@
 const express = require("express");
+// const { Server } = require("socketio");
+
 const router = express.Router();
 
 function main() {
   router.get("/", (req, res) => {
     res.render("index");
+  });
+
+  router.get("/socket", (req, res) => {
+    res.render("socketio");
   });
 }
 
