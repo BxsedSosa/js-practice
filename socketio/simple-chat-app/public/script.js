5f9447ed08d38176bec4359a0fe70ba43b428b12
BxsedSosa <donovanxabad@gmail.com>
2025-02-11 10:33:49 -0800

    able to connect to different browers and chat

---
+++ socketio/simple-chat-app/public/script.js
@@ -0,0 +1,29 @@
+const socket = io("http://localhost:3000", {
+  auth: {
+    secret: "this is a secret",
+  },
+  query: {
+    life: 42,
+  },
+});
+
+socket.on("welcome", (data) => {
+  console.log(data);
+  socket.emit("thankYou", [4, 5, 6]);
+});
+
+socket.on("newClient", (data) => {
+  console.log(`Message to all clients... ID joined: ${data}`);
+});
+
+socket.on("msgFromServerToAllClients", (data) => {
+  document.getElementById("display").innerHTML +=
+    `<h5>User: ${data.id}</h5><li>${data.newMsg}</li>`;
+});
+
+document.getElementById("message").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = document.getElementById("msg").value;
+  document.getElementById("msg").value = "";
+  socket.emit("msgFromClientToServer", message);
+});
---
+++ socketio/simple-chat-app/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+const expressServer = app.listen(port);
+
+const socketio = require("socket.io");
+
+const io = socketio(expressServer, {});
+
+io.on("connect", (socket) => {
+  console.log(`${socket.id} has joined our server`);
+
+  socket.on("msgFromClientToServer", (newMessage) => {
+    io.emit("msgFromServerToAllClients", {
+      id: socket.id,
+      newMsg: newMessage,
+    });
+  });
+});
