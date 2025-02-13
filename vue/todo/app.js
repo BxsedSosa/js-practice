2c06295149b3769f00df0eef39d1a6f2008a4ba4
BxsedSosa <donovanxabad@gmail.com>
2025-02-13 07:55:55 -0800

    got bare bones working

---
+++ vue/todo/app.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+  data() {
+    return {
+      addTaskText: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push(this.addTaskText);
+      this.addTaskText = "";
+    },
+    deleteTask(idx) {
+      this.tasks.splice(idx, 1);
+    },
+  },
+}).mount("#todoTask");
