98731b203a20b6e9fbec335f523de9a91855d40b
BxsedSosa <donovanxabad@gmail.com>
2025-02-21 10:04:53 -0800

    ...

--- udemy-node/section-5/01-start/app.js
+++ udemy-node/section-5/01-start/app.js
@@ -1,7 +1,15 @@
-const http = require("http");
+const express = require("express");
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use("/add-product", (req, res, next) => {
+  console.log("next middle ware");
+  res.send("<h1>Add product page</h1>");
+});
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("next middle ware");
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000);
