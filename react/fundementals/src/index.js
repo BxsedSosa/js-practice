edc0cdbecbe308c44b6292d1f1fadafa80198b26
BxsedSosa <donovanxabad@protonmail.com>
2025-04-21 22:07:06 -0700

    ...

--- react/fundementals/src/index.js
+++ react/fundementals/src/index.js
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-	return <h2>My First Component</h2>;
-}
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Img />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Img = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg"
+    alt="The Let Them Theory"
+  />
+);
+const Title = () => <h2>The Let Them Theory</h2>;
+const Author = () => <h4>Mel Robbins</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
