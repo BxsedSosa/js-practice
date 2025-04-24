8314018913a4b5daa3678eb4e03237c10e2ef7ba
BxsedSosa <donovanxabad@gmail.com>
2025-04-24 13:49:21 -0700

    completed project

--- react/backroad-app/src/components/Nav.js
+++ react/backroad-app/src/components/Nav.js
@@ -1,65 +1,67 @@
-const Nav = () => {
-  return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <NavHeader />
-        <ul className="nav-links" id="nav-links">
-          {navLinks.map((link, idx) => (
-            <NavLink href={`#${link}`} text={link} key={idx + 1} />
-          ))}
-        </ul>
+import logo from "../images/logo.svg";
 
-        <ul className="nav-icons">
-          {navSocials.map((social, idx) => (
-            <NavSocial
-              href={`https://www.${social}.com`}
-              className={`fab fa-${social}`}
-              key={idx + 1}
-            />
-          ))}
-        </ul>
-      </div>
-    </nav>
-  );
+const Nav = () => {
+	return (
+		<nav className="navbar">
+			<div className="nav-center">
+				<NavHeader />
+				<ul className="nav-links" id="nav-links">
+					{navLinks.map((link, idx) => (
+						<NavLink href={`#${link}`} text={link} key={idx + 1} />
+					))}
+				</ul>
+
+				<ul className="nav-icons">
+					{navSocials.map((social, idx) => (
+						<NavSocial
+							href={`https://www.${social}.com`}
+							className={`fab fa-${social}`}
+							key={idx + 1}
+						/>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 const NavHeader = () => {
-  return (
-    <div className="nav-header">
-      <img src="../../images/logo.svg" className="nav-logo" alt="backroads" />
-      <button type="button" className="nav-toggle" id="nav-toggle">
-        <i className="fas fa-bars"></i>
-      </button>
-    </div>
-  );
+	return (
+		<div className="nav-header">
+			<img src={logo} className="nav-logo" alt="backroads" />
+			<button type="button" className="nav-toggle" id="nav-toggle">
+				<i className="fas fa-bars"></i>
+			</button>
+		</div>
+	);
 };
 
 const navLinks = ["home", "about", "services", "tours"];
 
 const NavLink = ({ href, text }) => {
-  return (
-    <>
-      <li>
-        <a href={href} className="nav-link">
-          {""}
-          {text}
-          {""}
-        </a>
-      </li>
-    </>
-  );
+	return (
+		<>
+			<li>
+				<a href={href} className="nav-link">
+					{""}
+					{text}
+					{""}
+				</a>
+			</li>
+		</>
+	);
 };
 
 const navSocials = ["facebook", "twitter", "squarespace"];
 
 const NavSocial = ({ href, className }) => {
-  return (
-    <li>
-      <a href={href} target="_blank" className="nav-icon">
-        <i className={className}></i>
-      </a>
-    </li>
-  );
+	return (
+		<li>
+			<a href={href} target="_blank" className="nav-icon">
+				<i className={className}></i>
+			</a>
+		</li>
+	);
 };
 
 export default Nav;
